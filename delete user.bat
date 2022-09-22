7de5d01d734b755e3c7c9a129a5708f4353a5f4a
Tony-Castro <114043633+Tony-Castro@users.noreply.github.com>
2022-09-21 23:18:10 -0700

    Update delete user.bat

--- delete user.bat
+++ delete user.bat
@@ -1,6 +1,6 @@
 :: This deletes the account local.user
 net user local.user /delete 
-:: This deletes the directory of local.user
+:: This deletes the directory of local.user if present
 rmdir /S /Q "C:\Users\local.user"
 :: This creates new user and password
 net user "local.user" "4Remote" /add
