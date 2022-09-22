551312cbe7c24387b3f1e699e92574d69a757113
Tony-Castro <114043633+Tony-Castro@users.noreply.github.com>
2022-09-21 22:23:15 -0700

    Add files via upload

---
+++ delete user.bat
@@ -0,0 +1,10 @@
+:: This deletes the account local.user
+net user local.user /delete 
+:: This deletes the directory of local.user
+rmdir /S /Q "C:\Users\local.user"
+:: This creates new user and password
+net user "local.user" "4Remote" /add
+WMIC USERACCOUNT WHERE "Name='local.user'" SET PasswordExpires=FALSE
+WMIC USERACCOUNT WHERE "Name='local.user'" SET Passwordchangeable=FALSE
+
+pause
